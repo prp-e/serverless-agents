b55602a89602b4eced7cda2f4978af0654d5b8e8
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-06-03 01:18:26 +0330

    working on setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='serverless-agents',
+    version='0.0.1', 
+    author='Muhammadreza Haghiri',
+    author_email='<haghiri75@gmail.com>',
+    url='https://github.com/prp-e/serverless-agents',
+    license='MIT',
+    description='Making AI agents with serverless architecture',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['openai'],
+    keywords = ['face detection', 'cv', 'computer-vision'],
+    classifiers = ["Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",]
+)
