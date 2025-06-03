6e51f96622ff6ebc5651b84dfd72a17855e78682
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-06-03 15:44:24 +0330

    fixed: API address

--- examples/example_ollama.py
+++ examples/example_ollama.py
@@ -1,6 +1,6 @@
 import os 
 
-os.environ['OPENAI_BASE_URL'] = "http://localhost:11434/api/v1"
+os.environ['OPENAI_BASE_URL'] = "http://localhost:11434/v1"
 os.environ['OPENAI_API_KEY'] = "ollama-doesnt-need-api-key"
 
 from serverless_agents import Agent
