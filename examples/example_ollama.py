850c4f69ffd1a2a84144ee019bb4dce0c635a02b
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-06-03 15:42:45 +0330

    finished

--- examples/example_ollama.py
+++ examples/example_ollama.py
@@ -6,3 +6,11 @@ os.environ['OPENAI_API_KEY'] = "ollama-doesnt-need-api-key"
 from serverless_agents import Agent
 
 agent = Agent("haghiri/xei:2b")
+
+@agent.tool("This tool calculates the average of three numbers.")
+def avg(a, b, c):
+    sum = a + b + c
+    return sum / 3.0
+
+result = agent.chat("What is the average of 10, 13 and 20?")
+print(result)
