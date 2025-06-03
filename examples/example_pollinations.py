8d0cb39ac82e303a3c0f4548c734fd8979a2b050
Muhammadreza Haghiri <haghiri75@gmail.com>
2025-06-03 14:52:09 +0330

    fixed: environment bug

--- examples/example_pollinations.py
+++ examples/example_pollinations.py
@@ -1,8 +1,10 @@
-from serverless_agents import Agent
 import os 
 
 base_url = "https://text.pollinations.ai/openai"
 os.environ['OPENAI_BASE_URL'] = base_url
+
+from serverless_agents import Agent
+
 
 agent = Agent("openai")
 
